922ea85f2afdbb913bbd6a751b58e8a20e0bff84
beginerbeginer <miyabi6125@gmail.com>
2021-05-30 15:59:31 +0900

    SwaggerUIにタイトルと説明を追加

--- src/main/java/dev/simpleboot/rest/SpringRest3Application.java
+++ src/main/java/dev/simpleboot/rest/SpringRest3Application.java
@@ -3,11 +3,15 @@ package dev.simpleboot.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(info = @Info(title = "Task API", version = "1.0.0", description = "これはTaskアプリケーションのAPIです"))
 @SpringBootApplication
 public class SpringRest3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRest3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRest3Application.class, args);
+    }
 
 }
